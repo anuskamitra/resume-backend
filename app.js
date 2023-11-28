a58761f53245074b15c892d187b18c263d27ee50
AnuskaMitra <anuskamitra2001@gmail.com>
2023-11-28 13:46:32 +0530

    netlify.toml is updated

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const connectDB=require("./config/db")
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(
   session({
     secret: "Thisisalittlesecret",
